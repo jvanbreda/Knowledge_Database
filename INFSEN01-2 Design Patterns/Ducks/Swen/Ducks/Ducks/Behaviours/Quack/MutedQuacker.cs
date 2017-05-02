620491c0907469647a594be4a188e05dab1f35ce
swenmeeuwes <swenmeeu@gmail.com>
2017-05-02 14:53:06 +0200

    Refactored MuteQuacker to MutedQuacker

--- INFSEN01-2 Design Patterns/Ducks/Swen/Ducks/Ducks/Behaviours/Quack/MutedQuacker.cs
+++ INFSEN01-2 Design Patterns/Ducks/Swen/Ducks/Ducks/Behaviours/Quack/MutedQuacker.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Ducks.Behaviours.Quack
 {
-    class MuteQuacker : IQuackBehaviour
+    class MutedQuacker : IQuackBehaviour
     {
         public void Quack()
         {
